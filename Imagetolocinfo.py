3b6d029f0478c7027dfd7574138adeae9aa22cd3
CL <cmll@mit.edu>
2018-09-16 05:20:31 -0400

    Rev 1 tag images

--- Imagetolocinfo.py
+++ Imagetolocinfo.py
@@ -1,17 +1,35 @@
 from watson_developer_cloud import VisualRecognitionV3
 import json
+import os
+import getLatLong as geotag
 
+#all photos must be stored in 'Photos' directory (same dir as code)
+
+tagged_images = [] #[(lat,long),roof_true probability, roof_false probability]
 
 visual_recognition = VisualRecognitionV3(
     '2018-03-19',
     iam_apikey='vFTSKJUVMqQs9h4mHY0wwqI9yPjYS6z1tbv_ZkCfV3SX')
 
-with open('flood_people_on_house.jpg', 'rb') as images_file:
-    classes = visual_recognition.classify(
-        images_file,
-        threshold='0.1',
-        owners=["me"]).get_result()
-        #classifier_ids=['default']).get_result()
-        
-    print(json.dumps(classes, indent=2))
+for file in os.listdir('Photos'):
+
+    direc = 'Photos/' + file
+
+    with open(direc, 'rb') as images_file:
+        classes = visual_recognition.classify(
+            images_file,
+            threshold='0.0',
+            owners=["me"]).get_result()
+            #classifier_ids=['default']).get_result()
+
+        roof_false = classes['images'][0]['classifiers'][0]['classes'][0]['score']
+        roof_true = classes['images'][0]['classifiers'][0]['classes'][1]['score']
+
+
+    (lat,long) = geotag.getLatLong(direc)
+
+    tagged_images.append([(lat,long),roof_true,roof_false])
+    
+
+
 
